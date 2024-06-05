6fa48b57ecf64857a63cc7c6df35f5affb95caf4
averyverylongname <2745365050@qq.com>
2024-06-05 14:56:52 +0800

    项目初始化

---
+++ src/test/java/com/example/reactshoppingmall/ReactShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
